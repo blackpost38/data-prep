0ef268a2bd119f40296e705db2497e2c9e51041a
cgall <cgall@talend.com>
2017-10-19 14:17:12 +0200

    * useless import delete

--- dataprep-actions/src/test/java/org/talend/dataprep/transformation/actions/duplication/DeduplicateTest.java
+++ dataprep-actions/src/test/java/org/talend/dataprep/transformation/actions/duplication/DeduplicateTest.java
@@ -29,7 +29,6 @@ import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
-import static org.junit.Assert.assertFalse;
 import static org.talend.dataprep.api.dataset.ColumnMetadata.Builder.column;
 import static org.talend.dataprep.transformation.actions.ActionMetadataTestUtils.getColumn;
 
